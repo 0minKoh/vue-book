292de89c6465a7b7d1d2ea401c69698d796cb0f3
0minKoh <esesang13@naver.com>
2022-04-15 09:56:14 +0900

    ~proxy까지

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const target = 'proxy 요청을 보낼 서버주소'
+
 module.exports = 
   defineConfig({
   transpileDependencies: true}) 
 module.exports = {
   chainWebpack: config => {
     config.plugins.delete('prefetch') //prefetch 삭제
+  },
+  devServer: {
+    port: 8080,
+    proxy: { //porxy 요청을 보낼 api 시작 부분
+      '^/api': {
+        target,
+        changeOrigin: true
+      }
+    }
   }
 }
 
